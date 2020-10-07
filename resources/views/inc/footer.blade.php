ad6efe7efb6466df226ca34763e61205cda75cb8
Ayeah Godlove <ayeahgodlove5@gmail.com>
2020-10-07 12:21:00 +0100

    setting up files

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="border-top footer bg-secondary">
+    <div class="container p-3">
+        <div class="row justify-content-center">
+            <div class="col-11 col-md-4">
+                <h4>About Us</h4>
+                <div class="well">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quasi tempora odio itaque qui exercitationem praesentium voluptates perferendis sit suscipit?
+                </div>
+            </div>
+            <div class="col-11 col-md-4">
+                <h4>Follow us</h4>
+                <ul>
+                    <li><i class="fa fa-arrow-up"></i></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="col-11 col-md-4"></div>
+        </div>
+    </div>
+    <div class="container text-center">
+        &copy; 2020 - MinistriesInCameroon - <a asp-area="" asp-controller="Home" asp-action="Privacy">Privacy</a>
+    </div>
+</footer>
---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,36 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+        <div class="container">
+            <a class="navbar-brand" href="/">GlobalOpportunities</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
+                <ul class="navbar-nav flex-grow-1">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/jobs">Job Opportunities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/scholarships">Scholarships</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/scholarships">Training &amp; Development</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/scholarships">Blog</a>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a class="nav-link btn btn-primary btn-sm text-white" href="/scholarships">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/scholarships">Register</a>
+                    </li> --}}
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
